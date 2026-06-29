f9b4319c966bdd9c78c1678bda97de2feab4e134
emeyers <ethan.meyers@gmail.com>
2026-06-29 11:18:30 -0400

    Added homework and class code for the first class.

---
+++ ClassMaterial/class_code/class_01.R
@@ -0,0 +1,89 @@
+
+# 1.1 Basic arithmetic 
+
+
+
+
+
+# 1.2 Assigning values to objects
+
+
+
+
+
+# 1.3 Number journey 
+
+
+
+
+
+# 2.1 Numeric, string and Boolean data types
+
+
+
+
+
+
+
+
+# 3.1 Functions 
+
+
+
+# 3.2 To get help information about a function or data set
+
+
+
+
+
+# 4.1 Comments
+# One can add comments to your code to describe what you are doing
+
+
+
+
+
+# 5.1 Vectors are ordered sequences of numbers or letters
+# The c() function is used to create vectors
+
+
+
+
+	
+# 5.2 One can access elements of a vector using square brackets []
+
+
+
+
+
+
+# 5.3 One can assign a sequence of numbers to a vector
+
+
+
+
+
+
+# 5.4 One can also apply functions to vectors
+
+
+
+
+
+
+
+# What is the index of the maximum value in the vector v?
+
+
+
+
+
+# 5.5 We can add (or subtract) two vectors of the same length
+
+
+
+
+
+
+
+
